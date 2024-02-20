f9a12f99aadf4c1d69e21f150d87d67a8b5ffcbd
aisha701 <160603863+aisha701@users.noreply.github.com>
2024-02-20 17:50:43 +0500

    Add files via upload

---
+++ xml/index.js
@@ -0,0 +1,10 @@
+//DOM 
+ let image=document.getElementById("image")
+ function changeimg(){
+    image.src="./image/2.jfif";
+    image.style.border="2px solid black";
+ }
+ function reverseimg(){
+    image.src="./image/1.jfif";
+    image.style.border="2px solid green";
+ }
